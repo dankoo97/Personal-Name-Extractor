df0e1bc3a55f7addcec10a7275940acc70976fe5
dankoo97 <dak.7997@gmail.com>
2022-03-21 16:26:18 -0400

    Added some non-funtional subprograms to Archivist.java

--- lib/src/main/java/edu/odu/cs/cs350/Archivist.java
+++ lib/src/main/java/edu/odu/cs/cs350/Archivist.java
@@ -1,10 +1,44 @@
 class Archivist {
     public static void main(String args[]) {
         Archivist archivist = new Archivist();
-        archivist.shell();
+        if (args.length() > 0)
+        {
+//            Try to run a command based on user CLI input using switch statement
+//            If unable throw Error
+        } else {
+//            Open up shell environment
+            archivist.shell();
+        }
     }
 
     public void shell() {
-        System.out.println("Entered shell");
+        boolean cont = true;
+
+        while (cont) {
+            System.out.println("Please enter a command. Enter \"help\" for a list of commands");
+            Scanner scan = new Scanner(System.in);
+
+            cont = false;
+        }
+    }
+
+    public static String helpCommands() {
+//        Extractions
+//        Extract from string
+//        Extract from file
+//        Extract from directory
+//
+//        Train program
+//        Given corrected output, train to match
+        return "";
+    }
+
+    public String extract(String s) {
+        return s;
+    }
+
+
+    public String extract(File f) {
+        return "";
     }
 }
