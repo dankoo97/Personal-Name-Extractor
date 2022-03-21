95b3addfafa67ca23f576b1a4bd71f9569f0d88f
dankoo97 <dak.7997@gmail.com>
2022-03-21 17:19:11 -0400

    Added empty basic functions to Block.java based on initial design draft outputMarkedNames() separateIntoTokens()

--- lib/src/main/java/edu/odu/cs/cs350/Block.java
+++ lib/src/main/java/edu/odu/cs/cs350/Block.java
@@ -5,4 +5,25 @@ import java.util.Collection;
 public class Block {
     String text;
     Collection<Token> tokens;
+
+    /*
+    * Constructor
+    */
+    public Block(String s) {
+        text = s;
+    }
+
+    /*
+    * Outputs marked names within text
+    */
+    public String outputMarkedNames() {
+        return text;
+    }
+
+    /*
+    * Separates text into tokens
+    */
+    public void separateIntoTokens() {
+
+    }
 }
