a48e2af00aea34b62fd3a663ef5fd1d176940edf
dankoo97 <dak.7997@gmail.com>
2022-03-21 16:14:01 -0400

    Added basic shell and main

---
+++ lib/src/main/java/edu/odu/cs/cs350/Archivist.java
@@ -0,0 +1,10 @@
+class Archivist {
+    public static void main(String args[]) {
+        Archivist archivist = new Archivist();
+        archivist.shell();
+    }
+
+    public void shell() {
+        System.out.println("Entered shell");
+    }
+}
