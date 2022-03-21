c1f48d5582ee207b5617ed26606b34dd38706ae3
dankoo97 <dak.7997@gmail.com>
2022-03-21 17:35:47 -0400

    Added initial fail tests for TestBlock.java

--- lib/src/test/java/edu/odu/cs/cs350/TestBlock.java
+++ lib/src/test/java/edu/odu/cs/cs350/TestBlock.java
@@ -1,4 +1,23 @@
 package edu.odu.cs.cs350;
 
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
 public class TestBlock {
+
+    @Test
+    void testConstructor(String s) {
+        fail("Not yet implemented");
+    }
+
+    @Test
+    void testOutputMarkedNames() {
+        fail("Not yet implemented");
+    }
+
+    @Test
+    void testSeparateIntoTokens() {
+        fail("Not yet implemented");
+    }
 }
