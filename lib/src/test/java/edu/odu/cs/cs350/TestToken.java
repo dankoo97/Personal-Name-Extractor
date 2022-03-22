ae6537e3d7d874daeb104ccfee1e9ededd16b2ff
dankoo97 <dak.7997@gmail.com>
2022-03-22 12:57:13 -0400

    Added some basic infrastructure to Token constructor test

--- lib/src/test/java/edu/odu/cs/cs350/TestToken.java
+++ lib/src/test/java/edu/odu/cs/cs350/TestToken.java
@@ -7,6 +7,10 @@ import static org.junit.jupiter.api.Assertions.fail;
 public class TestToken {
     @Test
     void testTokenConstructor() {
+        Token token = new Token("Token");
+
+        // Assert that a token created with whitespace throws an exception
+
         fail("Not yet implemented");
     }
 }
