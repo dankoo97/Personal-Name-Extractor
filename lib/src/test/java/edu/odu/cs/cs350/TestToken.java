02c52b92fd0d6cf85d4efbfc763744676e4398f4
dankoo97 <dak.7997@gmail.com>
2022-03-22 12:49:43 -0400

    Created token constructor test

--- lib/src/test/java/edu/odu/cs/cs350/TestToken.java
+++ lib/src/test/java/edu/odu/cs/cs350/TestToken.java
@@ -1,4 +1,12 @@
 package edu.odu.cs.cs350;
 
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.fail;
+
 public class TestToken {
+    @Test
+    void testTokenConstructor() {
+        fail("Not yet implemented");
+    }
 }
