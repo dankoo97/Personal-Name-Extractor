0e957480178f37b34b9f2cb85d37192ed579863d
dankoo97 <dak.7997@gmail.com>
2022-03-22 11:18:41 -0400

    Changed tokens from Collection into array

--- lib/src/main/java/edu/odu/cs/cs350/Block.java
+++ lib/src/main/java/edu/odu/cs/cs350/Block.java
@@ -1,10 +1,8 @@
 package edu.odu.cs.cs350;
 
-import java.util.Collection;
-
 public class Block {
     String text;
-    Collection<Token> tokens;
+    Token[] tokens;
 
     /*
      * Constructor
