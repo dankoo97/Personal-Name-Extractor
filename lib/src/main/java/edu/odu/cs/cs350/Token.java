0e4f28e0ceffb31ffbb2ab2418cd54bd6ee7e1a9
cs_ahull001 <ahull001@odu.edu>
2022-03-22 11:47:56 -0400

    added functions to Token class to help Block class

--- lib/src/main/java/edu/odu/cs/cs350/Token.java
+++ lib/src/main/java/edu/odu/cs/cs350/Token.java
@@ -4,4 +4,18 @@ public class Token {
     String rawToken;
     boolean TokenClassification;
     FeatureSet features;
+
+    /*
+     * returns if token is the beginning of name
+     */
+    public boolean isBeginningOfName() {
+        return false; // placeholder
+    }
+
+    /*
+     * returns if token is the end of a name
+     */
+    public boolean isEndOfName() {
+        return false; // placeholder
+    }
 }
