735876ac9a9b27d300fa320cf7feb23b28970267
dankoo97 <dak.7997@gmail.com>
2022-03-23 18:47:14 -0400

    Created test class for TagUtil.java

---
+++ lib/src/test/java/edu/odu/cs/cs350/namex/tools/TestTagUtil.java
@@ -0,0 +1,22 @@
+package edu.odu.cs.cs350.namex.tools;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.fail;
+
+public class TestTagUtil {
+    @Test
+    void testWrapStringByTag() {
+        fail("Not yet implemented");
+    }
+
+    @Test
+    void testUnwrapTags() {
+        fail("Not yet implemented");
+    }
+
+    @Test
+    void testUnwrapTag() {
+        fail("Not yet implemented");
+    }
+}
