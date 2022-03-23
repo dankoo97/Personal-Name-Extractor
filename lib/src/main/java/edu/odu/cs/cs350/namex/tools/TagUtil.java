a01cc6aa4f146d50da3ab223af65c31ccd71e370
dankoo97 <dak.7997@gmail.com>
2022-03-23 18:48:05 -0400

    Created empty functions to unwrap tags from string

--- lib/src/main/java/edu/odu/cs/cs350/namex/tools/TagUtil.java
+++ lib/src/main/java/edu/odu/cs/cs350/namex/tools/TagUtil.java
@@ -1,11 +1,22 @@
 package edu.odu.cs.cs350.namex.tools;
 
 public class TagUtil {
+    /*
+    * Wraps a string by the given tag
+    */
     public static String wrapStringByTag(String tag, String string) {
         return String.join("", new String[] {
                 "<", tag, ">",
                 string,
                 "</", tag, ">",
         });
+    }
+
+    public static String unwrapTags(String string) {
+        return "";
+    }
+
+    public static String unwrapTag(String tag, String string) {
+        return "";
     }
 }
