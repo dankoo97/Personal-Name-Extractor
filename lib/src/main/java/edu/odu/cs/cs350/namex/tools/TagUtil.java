74336775b18da490e0b4eae2b9f73212dad042f8
dankoo97 <dak.7997@gmail.com>
2022-03-23 16:51:24 -0400

    Changed wrapStringByTag function to use String.join

--- lib/src/main/java/edu/odu/cs/cs350/namex/tools/TagUtil.java
+++ lib/src/main/java/edu/odu/cs/cs350/namex/tools/TagUtil.java
@@ -2,6 +2,10 @@ package edu.odu.cs.cs350.namex.tools;
 
 public class TagUtil {
     public static String wrapStringByTag(String tag, String string) {
-        return "<" + tag + ">" + string + "</" + tag + ">";
+        return String.join("", new String[] {
+                "<", tag, ">",
+                string,
+                "</", tag, ">",
+        });
     }
 }
