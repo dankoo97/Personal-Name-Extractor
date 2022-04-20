aac9830f37dfd05033833308ef0335d54b4779fe
dankoo97 <dak.7997@gmail.com>
2022-04-20 16:33:54 -0400

    Created better javadoc descriptions

--- lib/src/main/java/edu/odu/cs/cs350/namex/tools/TagUtil.java
+++ lib/src/main/java/edu/odu/cs/cs350/namex/tools/TagUtil.java
@@ -4,9 +4,13 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class TagUtil {
-    /*
-    * Wraps a string by the given tag
-    */
+    /**
+     * Wraps a string with tags
+     *
+     * @param tag The given tag to wrap the string
+     * @param string The string to be wrapped
+     * @return The returned string wrapped with the given tags
+     */
     public static String wrapStringByTag(String tag, String string) {
         return String.join("", new String[] {
                 "<", tag, ">",
@@ -15,9 +19,12 @@ public class TagUtil {
         });
     }
 
-    /*
-    * Unwraps all tags from a string
-    */
+    /**
+     * Unwraps all tags from a string
+     *
+     * @param string The given string to remove all tags from
+     * @return The given string without tags
+     */
     public static String unwrapTags(String string) {
         StringBuilder result = new StringBuilder();
 
@@ -33,9 +40,13 @@ public class TagUtil {
         return result.toString();
     }
 
-    /*
-    * Unwraps all instances of a given tag from a string
-    */
+    /**
+     * Unwraps all instances of a given tag from a string
+     *
+     * @param tag The tag to remove
+     * @param string The string from which to search and remove the given tag
+     * @return The given string without tags
+     */
     public static String unwrapTag(String tag, String string) {
         StringBuilder result = new StringBuilder();
 
